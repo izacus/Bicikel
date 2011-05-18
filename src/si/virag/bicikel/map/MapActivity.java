2c3c24c6b1c9fab777ca8bb9bc5d82078fbbdb4c
Jernej Virag <jernej@virag.si>
2011-05-18 11:23:29 +0200

    Added map location handling

--- src/si/virag/bicikel/map/MapActivity.java
+++ src/si/virag/bicikel/map/MapActivity.java
@@ -1,5 +1,7 @@
 package si.virag.bicikel.map;
 
+import java.util.List;
+
 import si.virag.bicikel.R;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,11 +9,14 @@ import android.util.Log;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
 
 public class MapActivity extends com.google.android.maps.MapActivity
 {
 	private MapView mapView;
-
+	private MyLocationOverlay myLocation;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -19,17 +24,50 @@ public class MapActivity extends com.google.android.maps.MapActivity
 		setContentView(R.layout.map_activity);
 		
 		Bundle extras = getIntent().getExtras();
-		double lng = extras.getDouble("lng");
-		double lat = extras.getDouble("lat");
+		double lat = extras.getDouble("lng");
+		double lng = extras.getDouble("lat");
 		
 		mapView = (MapView) findViewById(R.id.map);
 		GeoPoint pt = new GeoPoint((int)(lng * 1E6), (int)(lat * 1E6));
 		
 		MapController controller = mapView.getController();
 		controller.setCenter(pt);
+		controller.setZoom(17);
+		
+		List<Overlay> overlays = mapView.getOverlays();
+		overlays.clear();
+		
+		// Add station overlay
+		IconOverlay overlay = new IconOverlay(getResources(), pt, R.drawable.cycling);
+		overlays.add(overlay);
+		
+		// Add user location overlay
+		myLocation = new MyLocationOverlay(this, mapView);
+		myLocation.enableCompass();
+		myLocation.enableMyLocation();
+		overlays.add(myLocation);
+		
+		mapView.invalidate();
+		mapView.setBuiltInZoomControls(true);
 		
 		Log.i(this.toString(), "Showing lat" + lat + ", lng " + lng);
 		
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		myLocation.disableCompass();
+		myLocation.disableMyLocation();
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		myLocation.enableCompass();
+		myLocation.enableMyLocation();
 	}
 
 	@Override
