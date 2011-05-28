c90376b4afbe5044cc4a99ec552a41c9431cfea4
mavrik <mavrik@localhost>
2011-05-28 20:50:46 +0200

    Added map information display, fixed landscape layouts

--- src/si/virag/bicikel/map/StationMarker.java
+++ src/si/virag/bicikel/map/StationMarker.java
@@ -5,13 +5,27 @@ public class StationMarker
 	private double latitude;
 	private double longtitude;
 	private String description;
+	private int free;
+	private int bikes;
 	
-	public StationMarker(double latitude, double longtitude, String description)
+	public StationMarker(double latitude, double longtitude, String description, int free, int bikes)
 	{
 		super();
 		this.latitude = latitude;
 		this.longtitude = longtitude;
 		this.description = description;
+		this.free = free;
+		this.bikes = bikes;
+	}
+
+	public int getFree()
+	{
+		return free;
+	}
+
+	public int getBikes()
+	{
+		return bikes;
 	}
 
 	public double getLatitude()
