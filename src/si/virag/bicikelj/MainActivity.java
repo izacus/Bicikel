bcfee3315310876e0f0932155c52e1de064f8883
Jernej Virag <jernej@virag.si>
2012-02-15 21:45:19 +0100

    Implemented action bar icons

--- src/si/virag/bicikelj/MainActivity.java
+++ src/si/virag/bicikelj/MainActivity.java
@@ -2,7 +2,6 @@ package si.virag.bicikelj;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Window;
 
 public class MainActivity extends FragmentActivity 
 {
@@ -10,7 +9,6 @@ public class MainActivity extends FragmentActivity
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
-    	requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         // Fragments use actionbar to show loading status
         setContentView(R.layout.main);
