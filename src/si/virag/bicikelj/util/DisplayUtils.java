e1249398603a6235aa25299b7885ced148a035ed
Jernej Virag <jernej@virag.si>
2012-03-05 19:23:48 +0100

    Implemented maps detail view

--- src/si/virag/bicikelj/util/DisplayUtils.java
+++ src/si/virag/bicikelj/util/DisplayUtils.java
@@ -1,19 +1,20 @@
 package si.virag.bicikelj.util;
 
+import java.util.Locale;
+
 public class DisplayUtils 
 {
-
 	public static String formatDistance(Float distance)
 	{
 		// Using german locale, because slovene locale is not available on all devices
 		// and germany uses same number format
 		if (distance < 1200)
 		{
-			return String.format("%,.1f", distance) + " m";
+			return String.format(Locale.GERMAN, "%,.1f", distance) + " m";
 		}
 		else
 		{
-			return String.format("%,.2f", distance / 1000) + " km";
+			return String.format(Locale.GERMAN, "%,.2f", distance / 1000) + " km";
 		}
 	}	
 }
