a1919c18616f117f90f297728581b9632bb86a93
Jernej Virag <jernej@virag.si>
2016-09-02 00:21:58 +0200

    A few more potential crash fixes.

--- src/si/virag/bicikelj/station_map/StationMapFragment.java
+++ src/si/virag/bicikelj/station_map/StationMapFragment.java
@@ -366,7 +366,7 @@ public class StationMapFragment extends Fragment implements GoogleMap.OnInfoWind
             ssb.append(" ");
             String bikesStr = String.valueOf(s.getAvailableBikes());
             ssb.append(bikesStr);
-            ssb.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.primary_dark)), ssb.length() - bikesStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ssb.setSpan(new ForegroundColorSpan(ctx.getResources().getColor(R.color.primary_dark)), ssb.length() - bikesStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             ssb.setSpan(new StyleSpan(Typeface.BOLD), ssb.length() - bikesStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
             // Free hint text
@@ -375,10 +375,10 @@ public class StationMapFragment extends Fragment implements GoogleMap.OnInfoWind
             ssb.append(" ");
             String freeStr = String.valueOf(s.getFreeSpaces());
             ssb.append(freeStr);
-            ssb.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.secondary)), ssb.length() - freeStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ssb.setSpan(new ForegroundColorSpan(ctx.getResources().getColor(R.color.secondary)), ssb.length() - freeStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             ssb.setSpan(new StyleSpan(Typeface.BOLD), ssb.length() - freeStr.length(), ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-            ssb.setSpan(new AbsoluteSizeSpan((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14.0f, getResources().getDisplayMetrics())),
+            ssb.setSpan(new AbsoluteSizeSpan((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14.0f, ctx.getResources().getDisplayMetrics())),
                     startOfNumberHint,
                     ssb.length(),
                     Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -388,7 +388,7 @@ public class StationMapFragment extends Fragment implements GoogleMap.OnInfoWind
                 ssb.append("\n");
                 String distanceString = DisplayUtils.formatDistance(s.getDistance());
                 ssb.append(distanceString);
-                ssb.setSpan(new AbsoluteSizeSpan((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 11.0f, getResources().getDisplayMetrics())),
+                ssb.setSpan(new AbsoluteSizeSpan((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 11.0f, ctx.getResources().getDisplayMetrics())),
                         ssb.length() - distanceString.length(),
                         ssb.length(),
                         Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
