5aa042a7177b007b8766a2b7c68d0c7db93d92a4
jernej <jernej@129.88.168.192.in-addr.arpa>
2017-08-07 10:52:36 +0200

    Fix lint warnings

--- src/si/virag/bicikelj/util/GPSUtil.java
+++ src/si/virag/bicikelj/util/GPSUtil.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.Dialog;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApi;
 
 public class GPSUtil {
     public static final int GPS_FAIL_DIALOG_REQUEST_CODE = 9584;
@@ -15,15 +17,16 @@ public class GPSUtil {
         if (hasPlayServices)
             return true;
 
-        int error = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
+        GoogleApiAvailability availability = GoogleApiAvailability.getInstance();
+        int error = availability.isGooglePlayServicesAvailable(context);
 
         if (error == ConnectionResult.SUCCESS) {
             hasPlayServices = true;
             return true;
         }
 
-        if (GooglePlayServicesUtil.isUserRecoverableError(error)) {
-            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(error, context, GPS_FAIL_DIALOG_REQUEST_CODE);
+        if (availability.isUserResolvableError(error)) {
+            Dialog errorDialog = availability.getErrorDialog(context, error, GPS_FAIL_DIALOG_REQUEST_CODE);
             errorDialog.show();
             return false;
         }
